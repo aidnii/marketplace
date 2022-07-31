57c7b1385571a17bf3095488a83eac898db689b4
cryptichic <syberchief@gmail.com>
2022-07-31 16:56:31 -0500

    added the createMarketItem functionality

--- her-marketplace/contracts/NFTMarket.sol
+++ her-marketplace/contracts/NFTMarket.sol
@@ -71,4 +71,20 @@ contract NFTMarket is ReentrancyGuard {
             false
         );
     }
+
+    function createMarketItem(
+        address nftContract, 
+        uint256 itemId
+    ) public payable nonReentrant {
+        uint256 price = idToMarketItem[itemId].price;
+        uint256 tokenId = idToMarketItem[itemId].tokenId;
+        require(msg.value == price, "Please submit the asking price in order to complete the purchase");
+
+        idToMarketItem[itemId].seller.transfer(msg.value);
+        IERC721(nftContract).transferFrom(address(this), msg.sender, tokenId);
+        idToMarketItem[itemId].owner = payable(msg.sender);
+        idToMarketItem[itemId].sold = true;
+        _itemsSold.increment();
+        payable(owner).transfer(listingPrice);
+    }
 }
